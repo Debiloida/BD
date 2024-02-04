14d908d10a470f6658edcd5536d0a60991cc62c2
Kirill Sunozov <sunozov2006@bk.ru>
2024-02-05 01:26:06 +0300

    comment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Users from "./components/Users";
+import AddUser from "./components/AddUser";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: [
+            {
+                id: 1,
+                firstname:'Kirill',
+                lastname:'Sunozov',
+                bio:'Bio',
+                age: 17,
+                isHappy: true
+            },
+            {
+                id: 2,
+                firstname:'Kirill',
+                lastname:'Sunozov',
+                bio:'Bio',
+                age: 17,
+                isHappy: true
+            }
+        ]
+    }   
+    this.addUser = this.addUser.bind(this)
+ }
+    render() {
+        return (<div >
+            <Header title="Список пользователей" />
+            <main>
+                <Users users={this.state.users}/>
+            </main>
+            <aside>
+                <AddUser onAdd={this.addUser} />
+            </aside>
+        </div>)
+    }
+    addUser(user) {
+        const id= this.state.length + 1
+        this.setState({users: [...this.state.users, {id, ...user}]})
+       }
 }
 
-export default App;
+
+export default App
